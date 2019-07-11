3ee4d5ac60251545bdb41beca0960a62263842e2
Masoud Sahebolamri <masoudamri@gmail.com>
2019-07-11 01:53:00 -0400

    schema constraint working

--- monty/persistence/src/main/java/com/ors/junk/monty/persistence/service/impl/PersistenceServiceImpl.java
+++ monty/persistence/src/main/java/com/ors/junk/monty/persistence/service/impl/PersistenceServiceImpl.java
@@ -4,69 +4,92 @@ import java.util.UUID;
 
 import com.orientechnologies.orient.core.db.ODatabaseType;
 import com.orientechnologies.orient.core.db.OrientDBConfig;
-import com.orientechnologies.orient.core.db.object.ODatabaseObject;
 import com.orientechnologies.orient.core.id.ORID;
+import com.orientechnologies.orient.core.metadata.schema.OClass.INDEX_TYPE;
+import com.orientechnologies.orient.object.db.OObjectDatabasePool;
+import com.orientechnologies.orient.object.db.OObjectDatabaseTx;
 import com.orientechnologies.orient.object.db.OrientDBObject;
 import com.ors.junk.monty.persistence.model.Persistable;
+import com.ors.junk.monty.persistence.model.PlayerEntity;
 import com.ors.junk.monty.persistence.service.PersistenceService;
 
 public class PersistenceServiceImpl implements PersistenceService {
+	private OObjectDatabasePool db;
 
-	private final OrientDBObject orientDb;
-	private ODatabaseObject db;
+	static {
+		OrientDBObject orientDb = new OrientDBObject("plocal:junk", OrientDBConfig.defaultConfig());
+		if (orientDb.exists("monty")) {
+			orientDb.drop("monty");
+		}
+		orientDb.create("monty", ODatabaseType.PLOCAL);
+		orientDb.close();
+
+		OObjectDatabasePool db = new OObjectDatabasePool("plocal:junk/monty", "admin", "admin");
+
+		OObjectDatabaseTx tx = db.acquire();
+
+		tx.setAutomaticSchemaGeneration(true);
+		tx.getEntityManager().registerEntityClasses(Persistable.class.getPackage().getName());
+		tx.getMetadata().getSchema().synchronizeSchema();
+		tx.getMetadata().getSchema().getClass(PlayerEntity.class).getProperty("name").createIndex(INDEX_TYPE.UNIQUE);
+		System.out.println(tx.getMetadata().getSchema().getClass(PlayerEntity.class).getProperty("name"));
+		tx.commit();
+
+	}
 
 	public PersistenceServiceImpl() {
-		// orientDB = new OrientDBObject("plocal:monty",
-		// OrientDBConfig.defaultConfig());
-		// db = orientDB.open("monty", "admin", "admin_passwd");
 
-		orientDb = new OrientDBObject("memory:monty", OrientDBConfig.defaultConfig());
-		orientDb.create("monty", ODatabaseType.MEMORY);
-
-		db.getEntityManager().registerEntityClasses(Persistable.class.getPackage().getName());
-		db.commit();
+		db = new OObjectDatabasePool("plocal:junk/monty", "admin", "admin");
 	}
 
 	@Override
 	public <T extends Persistable> T newEntity(Class<T> newEntityClass) {
+		OObjectDatabaseTx tx = null;
 		try {
-			db.begin();
-			T object = (T) db.newInstance(newEntityClass);
-			return db.save(object);
+			tx = db.acquire().begin();
+			T object = (T) tx.newInstance(newEntityClass);
+			return tx.save(object);
 		} finally {
-			db.commit();
+			tx.commit();
 		}
 	}
 
 	@Override
 	public <T extends Persistable> void update(T entity) {
+		OObjectDatabaseTx tx = null;
 		try {
-			db.begin();
-			db.save(entity);
+			tx = db.acquire().begin();
+			tx.save(entity);
 		} finally {
-			db.commit();
+			tx.commit();
 		}
 	}
 
 	@Override
 	public <T extends Persistable> T get(ORID id, Class<T> entity) {
+		OObjectDatabaseTx tx = null;
 		try {
-			db.begin();
-			return db.load(id);
+			tx = db.acquire().begin();
+			return tx.load(id);
 		} finally {
-			db.commit();
+			tx.commit();
 		}
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public <T extends Persistable> T find(UUID id, Class<T> entityClass) {
-		return (T) db.query(String.format("select from %s where id=?", entityClass.getSimpleName()), id);
+		OObjectDatabaseTx tx = null;
+		try {
+			tx = db.acquire().begin();
+			return (T) tx.query(String.format("select from %s where id=?", entityClass.getSimpleName()), id);
+		} finally {
+			tx.commit();
+		}
 	}
 
 	@Override
 	public <T extends Persistable> boolean delete(UUID id, Class<T> entity) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
--- monty/persistence/src/test/java/com/ors/junk/monty/persistence/service/impl/PersistenceServiceImplTest.java
+++ monty/persistence/src/test/java/com/ors/junk/monty/persistence/service/impl/PersistenceServiceImplTest.java
@@ -1,34 +1,38 @@
 package com.ors.junk.monty.persistence.service.impl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.orientechnologies.orient.core.storage.ORecordDuplicatedException;
 import com.ors.junk.monty.persistence.model.PlayerEntity;
 import com.ors.junk.monty.persistence.service.PersistenceService;
 
 public class PersistenceServiceImplTest {
-	
-	PersistenceService persistenceService=new PersistenceServiceImpl();
-	
+
+	PersistenceService persistenceService = new PersistenceServiceImpl();
+
 	@Test
 	public void simplePlayerTest() {
-		PlayerEntity player=persistenceService.newEntity(PlayerEntity.class);
+		PlayerEntity player = persistenceService.newEntity(PlayerEntity.class);
 		player.setName("mike");
-	    persistenceService.update(player);
+		persistenceService.update(player);
 
 		System.out.println(persistenceService.get(player.getOrId(), PlayerEntity.class).getName());
 	}
 
 	@Test
 	public void simplePlayerTest2() {
-		PlayerEntity player=persistenceService.newEntity(PlayerEntity.class);
-		player.setName("mike");
-	    persistenceService.update(player);
+		Assertions.assertThrows(ORecordDuplicatedException.class, () -> {
+			PlayerEntity player = persistenceService.newEntity(PlayerEntity.class);
+			player.setName("mike");
+			persistenceService.update(player);
 
-		PlayerEntity player2=persistenceService.newEntity(PlayerEntity.class);
-		player2.setName("mike");
-	    persistenceService.update(player);
+			PlayerEntity player2 = persistenceService.newEntity(PlayerEntity.class);
+			player2.setName("mike");
+			persistenceService.update(player2);
 
-		System.out.println(persistenceService.get(player.getOrId(), PlayerEntity.class).getName());
+			System.out.println(persistenceService.get(player.getOrId(), PlayerEntity.class).getName());
+		});
 	}
 
 }
